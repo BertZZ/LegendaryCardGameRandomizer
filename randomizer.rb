1767d5464e9a8eff19ce00a3f8bc5ddc6fdf732e
BertZZ <bertie.wooles@hotmail.co.uk>
2018-05-27 22:37:14 +0100

    Adds Henchmen randomization

--- randomizer.rb
+++ randomizer.rb
@@ -1,7 +1,13 @@
 class Randomizer
 
   def initialize
-  @selected = []
+    @masterminds = ["Skull","Magneto","Loki","Doom"]
+    @schemes = ["Portals to the Dark Dimension","Civil War","Cosmic Cube","Virus","Bank Robbery","Skrull Shapeshifters","Killbots","Prision Breakout"]
+    henchmen = ["Doombot Legion","Hand Ninja's","Savage Land Mutates","Sentinals"]
+    villan_groups = ["Brotherhood","Hydra","Enemies of Asgard", "Masters of Evil", "Radiation","Skruls","Spider-Foes"]
+    @mastermind = ' '
+    @scheme = ' '
+    @chosen = []
   end
 
   def randomize_heroes
@@ -10,21 +16,43 @@ class Randomizer
   end
 
   def randomize_maserminds
-    masterminds = ["Skull","Magneto","Loki","Doom"]
-    puts masterminds.sample
+    @mastermind = @masterminds.sample
+    puts @mastermind
   end
 
   def randomize_schemes
-    schemes = ["Portals to the Dark Dimension","Civil War","Cosmic Cube","Virus","Bank Robbery","Skrull Shapeshifters","Killbots","Prision Breakout"]
-    puts schemes.sample
+    @scheme = @schemes.sample
+    puts @scheme
   end
 
-  def setup
+  def randomize_henchmen(players)
+    henchmen = ["Doombot Legion","Hand Ninja's","Savage Land Mutates","Sentinals"]
+    chosen_henchmen = []
+    if @mastermind == "Doom"
+      henchmen.delete("Doombot Legion")
+      chosen_henchmen << "Doombot Legion"
+    end
+
+    if players < 4 && chosen_henchmen.length == 0
+      hench = henchmen.sample
+      chosen_henchmen << hench
+    elsif players >= 4 && chosen_henchmen.length == 1
+      hench = henchmen.sample
+      chosen_henchmen << hench
+    elsif  players >= 4 && chosen_henchmen.length == 0
+      hench = henchmen.sample(2)
+      chosen_henchmen << hench
+    end
+    puts chosen_henchmen
+  end
+
+  def setup(players)
     randomize_heroes
     randomize_maserminds
     randomize_schemes
+    randomize_henchmen(players)
   end
 end
 
 randomizer = Randomizer.new
-randomizer.setup
+randomizer.setup(4)
